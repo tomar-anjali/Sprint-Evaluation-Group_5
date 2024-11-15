0a9628d90935f4981adccfc1d2762fdc8ecd6d04
sunaina <sunaina11sadare@gmail.com>
2024-11-15 18:17:54 +0530

    Updated

--- src/main/java/com/evaluation/petshop/models/dto/ResponseStructure.java
+++ src/main/java/com/evaluation/petshop/models/dto/ResponseStructure.java
@@ -1,13 +1,9 @@
 package com.evaluation.petshop.models.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+
 
 @Data
-
 public class ResponseStructure<T> {
 	private int statusCode;
 	private String message;
